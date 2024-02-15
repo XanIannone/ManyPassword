69c5b7e171d9b1e918e8f473f57efe3000e17e4f
XanIannone <xan.iannone@gmail.com>
2024-02-15 13:40:44 -0500

    Update Classes.py

---
+++ Modules/Classes.py
@@ -0,0 +1,25 @@
+
+class Generator:
+    def scramble_password(self, unscrambled_string):
+        import random
+        string = list(unscrambled_string)
+        random.shuffle(string)
+        return ''.join(string)
+
+class SaveAndView:
+    def view_passwords(self):
+        from data.Saved_Passwords import saved
+        for password in saved:
+            print(f"{password}: {saved[password]}\n")
+    def save_passwords(self, password_list):
+        global password
+        global reader
+        from data.Saved_Passwords import saved
+        reader = open('Saved_Passwords.py', 'w')
+        for password in password_list:
+            saved[password] = password_list[password]
+        reader.write(f"saved = {saved}")
+        reader.close()
+
+
+
