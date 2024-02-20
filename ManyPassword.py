3b4f508bcd30881c3d0fa6fc223e9cbd2da7e895
XanIannone <xan.iannone@gmail.com>
2024-02-19 20:46:06 -0500

    Update ManyPassword.py

--- ManyPassword.py
+++ ManyPassword.py
@@ -1,6 +1,10 @@
 import random
 from modules.data.art import logo
-from modules.Password_Generating import 
+from modules.classes import Generator, SaveView
+
+password_generator = Generator()
+password_saveview = SaveView()
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
