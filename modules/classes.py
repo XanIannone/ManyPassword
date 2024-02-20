3ea91f03b1c3a00ab35f92ad0dca07089951ca5a
XanIannone <xan.iannone@gmail.com>
2024-02-19 20:46:11 -0500

    Update classes.py

--- modules/classes.py
+++ modules/classes.py
@@ -5,17 +5,19 @@ class Generator:
         random.shuffle(string)
         return ''.join(string)
 
+
 class SaveView:
     def view_passwords(self):
         from data.Saved_Passwords import saved
         for password in saved:
             print(f"{password}: {saved[password]}\n")
+
+
     def save_passwords(self, password_list):
-        global password
-        global reader
         from data.Saved_Passwords import saved
         reader = open('Saved_Passwords.py', 'w')
         for password in password_list:
             saved[password] = password_list[password]
         reader.write(f"saved = {saved}")
         reader.close()
+
